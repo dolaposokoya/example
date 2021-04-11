e0c0ee03e3c32252e243d4419bd5544647b1a3a2
dolaposokoya <dolaposokoya97@gmail.com>
2021-04-11 21:06:39 +0530

    new

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,6 +5,9 @@ const router = express.Router();
 router.get('/getUser', (req, res) => {
     res.send({ name: 'John', age: 20 })
 })
+router.post('/register', (req, res) => {
+    res.send('Register yourself')
+})
 
 
 export = router;
